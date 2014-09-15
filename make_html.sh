5393f6596b9cd864d095c1f81c74453a7984c193
sakuramochi <sakuramochi.mochi.0@gmail.com>
2014-09-16 01:47:17 +0900

    Make use of appropriate options of pandoc.

--- make_html.sh
+++ make_html.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 for i in *.md; do
-    pandoc -t html5+lhs -H default-header -H ${i:r}-header ${i} -o html/${i:r}.html
+    pandoc -t html5+lhs -V lang=ja -V css=default.css -H ${i:r}-header ${i} -o html/${i:r}.html
 done
 
