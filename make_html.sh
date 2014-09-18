4ec9847f3150b5b129cb3559cf0d911679bd7450
sakuramochi <sakuramochi.mochi.0@gmail.com>
2014-09-18 13:11:29 +0900

    Added font-awesome and default-header.

--- make_html.sh
+++ make_html.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 for i in *.md; do
-    pandoc -t html5+lhs -V lang=ja -V css=default.css -H ${i:r}-header ${i} -o html/${i:r}.html
+    pandoc -t html5+lhs -V lang=ja -V css=default.css -V css=http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css -H default-header -H ${i:r}-header ${i} -o html/${i:r}.html
 done
 
