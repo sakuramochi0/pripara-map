728a6b413251215c180173f89c22939c30311e0c
sakuramochi <sakuramochi.mochi.0@gmail.com>
2015-01-28 10:44:41 +0900

    Update

--- make_html.py
+++ make_html.py
@@ -9,33 +9,60 @@ with open('google-map-link.csv') as f:
     
     links = []
     pre_region = ''
-    for pref, pref_name, region, mid in reader:
+    for pref, pref_name, region, mid, ll, z in reader:
         if region != pre_region:
             links.append('\n### {}'.format(region))
             pre_region = region
         links.append('* [{}]({})'.format(pref, pref_name))
 
         # make pref pages
-        with open('pref-template') as f:
+        with open('html-templates/pref-template') as f:
             template = f.read()
-        html = template.format(pref=pref, pref_name=pref_name, mid=mid)
-        filename = 'htmls/' + pref_name + '.md'
+        html = template.format(pref=pref, pref_name=pref_name, mid=mid, ll=ll, z=z)
+        filename = 'html-templates/' + pref_name + '.md'
         with open(filename, 'w') as f:
             f.write(html)
 
         # header
-        with open('pref-header-template') as f:
+        with open('html-templates/pref-header-template') as f:
             template = f.read()
         html = template.format(pref=pref)
-        filename = 'htmls/' + pref_name + '-header'
+        filename = 'html-templates/' + pref_name + '-header'
         with open(filename, 'w') as f:
             f.write(html)
 
-    with open('index-template') as f:
-        template = f.read()
-    with open('store_num.txt') as f:
-        store_num = f.read()
-    html = template.format(links='\n'.join(links), store_num=store_num)
-    with open('htmls/index.md', 'w') as f:
-        f.write(html)
+with open('html-templates/index-template') as f:
+    template = f.read()
+with open('store_num.txt') as f:
+    store_num = f.read()
+html = template.format(links='\n'.join(links), store_num=store_num)
+with open('html-templates/index.md', 'w') as f:
+    f.write(html)
+
+with open('google-map-link-old.csv') as f:
+    reader = csv.reader(f)
+    
+    for prefs, pref_names in reader:
+        
+        links = []
+
+        for pref, en in zip(prefs.split('/'), pref_names.split('-')):
+
+            links.append('* [{}]({})'.format(pref, en))
+
+            # make pref pages
+            with open('html-templates/pref-template-old') as f:
+                template = f.read()
+            html = template.format(pref=prefs, pref_name=pref_names, links='\n'.join(links))
+            filename = 'html-templates/' + pref_names + '.md'
+            with open(filename, 'w') as f:
+                f.write(html)
+
+            # header
+            with open('html-templates/pref-header-template') as f:
+                template = f.read()
+            html = template.format(pref=prefs)
+            filename = 'html-templates/' + pref_names + '-header'
+            with open(filename, 'w') as f:
+                f.write(html)
 
--- make_html.sh
+++ make_html.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 ./make_html.py
-cd htmls
+cd html-templates
 for i in *.md; do
     pandoc -t html5+lhs -V lang=ja -V css=default.css -V css=http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css -H default-header -H ${i:r}-header ${i} -o ../html/${i:r}.html
 done
